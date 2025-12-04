6041dda55a714c8a7fbb8240514380872d63ac06
Prince-Singh-05 <luciefur6393473659@gmail.com>
2025-12-04 23:29:42 +0530

    added docker compose file without secrets

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,3 +4,7 @@ export default async function Page() {
   const users = await prisma.user.findMany();
   return <div className="p-4">{JSON.stringify(users)}</div>;
 }
+
+export const revalidate = 60; // Revalidate every 60 seconds
+// or
+// export const dynamic = 'force-dynamic'; // Always fetch fresh data (dynamic-page)
